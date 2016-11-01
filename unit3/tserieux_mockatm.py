8de79eb4fe4e6539adadc9cdabbb572e1ca9d849
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-11-01 16:15:27 +0000

    guessing game

--- unit3/tserieux_mockatm.py
+++ unit3/tserieux_mockatm.py
@@ -1,11 +1,14 @@
 bank_account = 10000
 print("1.Withdraw 2.Deposite 3.Exit")
 choice = input("Welcome to ATM! Pick from above [1|2|3]: ")
-while choice != "3":
+while choice != "3" and bank_account > 0:
     if choice == "1":
         withdraw = int(input("How much money would you like to withdraw "))
-        bank_account = bank_account - withdraw
-        print(bank_account)
+        if withdraw > bank_account:
+            print("Insufficient Funds")
+        elif withdraw < bank_account:
+            bank_account = bank_account - withdraw
+            print(bank_account)
     elif choice == "2":
         deposit =int(input("How much money would you like to deposit "))
         bank_account = bank_account + deposit
