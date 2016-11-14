be2997b201822c64f7a17b2b63910d15a201aadd
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-11-14 17:15:53 +0000

    rps

--- unit3/tserieux_numguess.py
+++ unit3/tserieux_numguess.py
@@ -1,17 +1,17 @@
 import random 
 
 num = random.randint(1,100)
-print (num)
+
 guess = int(input("What do you think the number is "))
 attempts = 0
 while guess != num:
-   if guess >= num :
-        attempts = guess +1
-        print (int(input(("to high try again "))))
-   elif guess <= num:
-        attempts = guess + 1
-        print (int(input(("To low try again "))))
-   else:
+    if num > guess:
+        attempts = attempts + 1
+        guess = (int(input("too low try again ")))
+    elif num < guess:
+        attempts = attempts +1
+        guess =(int(input("to high try again ")))
+    else:
         break
 print("you did it the number is {} it took you {} attempts".format(num,attempts))
             
