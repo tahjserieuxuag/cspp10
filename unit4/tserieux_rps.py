aaef28a3f4dc3e10c79d88e69019ba8634cff77c
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-11-16 17:15:45 +0000

    Labs

--- unit4/tserieux_rps.py
+++ unit4/tserieux_rps.py
@@ -6,7 +6,7 @@ import random
 
 
 def get_p1_move():
-    p1_move = input("Would you like to play Rock, Paper or Scissors")
+    p1_move = input("Would you like to play Rock, Paper or Scissors ")
     if p1_move == "Rock":
         return "Rock"
     elif p1_move == "Paper":
@@ -14,13 +14,16 @@ def get_p1_move():
     elif p1_move == "Scissors":
         return "Scissors"
 def get_comp_move():
-    comp_move = random.randint(1,3)
-    if comp_move == 1:
+    comp_move.lower = random.randint(1,3)
+    if comp_move.lower == 1:
         return "Rock"
-    elif comp_move == 2:
+    elif comp_move.lower == 2:
         return "Paper"
-    elif comp_move == 3:
+    elif comp_move.lower == 3:
         return "Scissors"
-
+def get_round():
+    rounds = int(input("how many rounds would you like to play from 1 to 9 "))
+    return rounds
 print(get_comp_move())
 print(get_p1_move())
+print(get_round())
