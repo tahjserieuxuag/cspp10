84403afcf4abdf707ee88e27c0bef22ed6734d3a
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-11-29 17:15:46 +0000

    lab

--- unit4/tserieux_rps.py
+++ unit4/tserieux_rps.py
@@ -4,11 +4,8 @@ import random
 
 
 
-player_wins = 0
-comp_wins = 0
-ties = 0
 def get_round():
-    rounds = int(input("how many rounds would you like to play from 1 to 9 "))
+    rounds = int(input("how many rounds would you like to play from 1 to 9: "))
     return (rounds + 1)
 def get_p1_move():
     p1_move = input("Would you like to play Rock, Paper or Scissors ")
@@ -28,48 +25,44 @@ def get_comp_move():
         return "Scissors"
 def get_winner(p1_move,comp_move):
     if p1_move == comp_move:
-        ties = ties +1
         return "tie"
-    elif p1_move == "rock" and comp_move == "Paper":
-        player_wins == player_wins +1
+    elif p1_move == "Rock" and comp_move == "Paper":
+        return "Computer"
+    elif p1_move == "Rock" and comp_move == "Scissors":
         return "player"
-    elif p1_move == "rock" and comp_move == "Scissors":
-        player_wins == player_wins + 1
-        return "player"
-    elif p1_move == "paper" and comp_move == "Rock":
-        comp_wins == comp_wins + 1
+    elif p1_move == "Paper" and comp_move == "Rock":
         return "computer"
-    elif p1_move == "paper" and comp_move == "Scissors":
-        comp_wins == comp_wins +1
+    elif p1_move == "Paper" and comp_move == "Scissors":
         return "computer"
-    elif p1_move == "scissors" and comp_move == "Rock":
-        comp_wins == comp_wins + 1
+    elif p1_move == "Scissors" and comp_move == "Rock":
         return "computer"
-    elif p1_move == "scissors" and comp_move == "Paper":
-        player_wins == player_wins + 1
+    elif p1_move == "Scissors" and comp_move == "Paper":
         return "player"
 def print_score(player_wins,comp_wins,ties):
-    print("The player's score is {}".format(player_wins))
-    print("The computer's score is {}".format(comp_wins))
-    print("There were {} ties".format(ties))
+    print("The player's score is: {}".format(player_wins))
+    print("The computer's score is: {}".format(comp_wins))
+    print("There are {} tie".format(ties))
 def rps():
+    player_wins = 0
+    comp_wins = 0
+    ties = 0
     rounds = get_round()
-    p1move = get_p1_move()
-    compmove = get_comp_move()
-    winner = get_winner(p1move,compmove)
-    for x in range(1,rounds):
-        print("Player chose {}".format(p1_move))
-        print("Computer chose{}".format(get_comp_move()))
+    for x in range(rounds):
+        p1move = get_p1_move()
+        compmove = get_comp_move()
+        winner = get_winner(p1move,compmove)
+        print("Player chose: {}".format(p1move))
+        print("Computer chose: {}".format(compmove))
         if winner == "player":
             print("Player Won!")
+            player_wins = player_wins + 1
         elif winner == "computer":
             print("Computer Won!")
+            comp_wins = comp_wins + 1
         else:
             print("Tie!")
+            ties = ties + 1
+        print_score(player_wins,comp_wins,ties)
+
 rps()
-print_score(player_wins,comp_wins,ties)
     
-
-
-
-
