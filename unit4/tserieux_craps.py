82be71e5417b2c0e720259b7314f05f5dd4dc558
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-12-09 17:12:26 +0000

    LabsCraps

--- unit4/tserieux_craps.py
+++ unit4/tserieux_craps.py
@@ -2,7 +2,7 @@ import random
 Player_mone = 100
 
 def get_bet():
-    bet = int(input("How much would you like to bet "))
+    bet = int(input("How much would you like to bet? "))
     return bet
     
 def roll2dice():
@@ -11,5 +11,31 @@ def roll2dice():
     dice_sum = dice1 + dice2
     print("you rolled {},{} and {} in total".format(dice1,dice2,dice_sum))
     return dice_sum
+
+def phase3 (dice_sum):
+    phase3_dice = 0
+    dice_sum = phase3_dice
+    phase_dice = random.randint(1,6)
+    phase_dice1 = random.randint(1,6)
+    phase_sum = phase_dice + phase_dice1
+    print("you rolled {},{} and {} in total".format(phase_dice,phase_dice1,phase_sum))
+    
+def bet_verificatoin(bet):
+    player_money = 100
+    if bet > player_money:
+        bet = input("too much ")
+
+    
+
+
+
+
+
+
+
+
+
+
 get_bet()
 roll2dice()
+phase3(dice_sum)
