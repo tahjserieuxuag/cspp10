6bc34d7a517c621a9ed1b61952ad08be2779dc4c
TahjSerieuxMeme <tahjserieux@uagateway.org>
2016-12-30 17:23:13 +0000

    This is difficult

---
+++ Test/tserieux_quickdraw.py
@@ -0,0 +1,38 @@
+import random
+
+def get_player_move():
+    bullets = 0
+    P_move = input("Will you BLOCK,RELOAD or SHOOT")
+    if p_move.lower() == "block":
+        return "block"
+    elif p_move.lower() == "shoot":
+        return "shoot"
+    elif p_move.lower() == "shoot" and bullet == 0:
+        return "no bullets"
+    elif p_move.lower() == "reload":
+        return "reload"
+    else:
+        p_move = input("That is not a move, input again")
+
+def get_comp_move():
+    cbullet = 0
+    comp_move = random.randint(1,3)
+    if comp_move == 1:
+        return "shoot"
+    elif comp_move == 1 and cbullets == 0:
+        return "no bullets"
+    elif comp_move == 2:
+        return "block"
+    elif comp_move == 3:
+        return "reload"
+
+def quick_draw(comp_move,p_move):
+    player_health = 3
+    computer_health = 3
+    while player_health != 0 or computer_health != 0:
+        if p1_move == "shoot" and comp_move == "reload":
+            computer_health = computer_health - 1
+        elif p1_move == "shoot" and comp_move == "shoot":
+            player_health = player_health - 1 and computer_health = computer_health - 1
+    
+        
--- unit4/tserieux_craps.py
+++ unit4/tserieux_craps.py
@@ -4,13 +4,14 @@ Player_mone = 100
 def get_bet():
     bet = int(input("How much would you like to bet? "))
     player_money = 100
-    if bet > player_money:
-        bet = input("too much ")
-    elif bet < 0:
-        bet = input("no negative number ")
-    elif bet == 0:
-        bet = input("zero is not an option ")
-    return bet
+    while bet>player_money or bet < 0 or bet == 0:
+        if bet > player_money:
+            bet = input("too much bet again ")
+        elif bet < 0:
+            bet = input("no negative number ")
+        elif bet == 0:
+            bet = input("zero is not an option, bet again ")
+        return bet
 # get a bet
   
 def roll2dice():
@@ -18,14 +19,28 @@ def roll2dice():
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     print("you rolled {},{} and {} in total".format(dice1,dice2,dice_sum))
+    if dice_sum == 7 or dice_sum == 1:
+        return "Winner"
+    elif dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        return "Loose"
     return dice_sum
+    
 
 def phase3(dice_sum):
     while True:
         pdice1 = random.randint(1,6)
         pdice2 = random.randint(1,6)
         phase_sum = pdice1 + pdice2
-        if phase_sum = dice_sum:
+        if phase_sum == dice_sum:
+            print("player wins")
+            return "win"
+        elif phase_sum == 7:
+            print("you lose")
+            return "lose"
+def craps():
+    
+            
+
         
 
 
@@ -41,5 +56,3 @@ def phase3(dice_sum):
 
 
 get_bet()
-roll2dice()
-dice_sum = roll2dice()
