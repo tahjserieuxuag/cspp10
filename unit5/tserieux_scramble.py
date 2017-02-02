f5f7949ac4c4fe4c6594d935eb3ab556f94b3895
TahjSerieuxMeme <tahjserieux@uagateway.org>
2017-02-02 17:14:05 +0000

    scramble

--- unit5/tserieux_scramble.py
+++ unit5/tserieux_scramble.py
@@ -1,4 +1,22 @@
+import random
+word = "Paris"
 #a function that scrambles one word
 def scramble_word(word):
+#turn the word into a list
     
+    if len(word) >= 4:
+        wlist = list(word)  
+        
+    #scramble everything but the first and last index
+        wlist = wlist[1:-1]
+        random.shuffle(wlist)
+        print(wlist)
     
+    #put word back together
+        
+        
+    
+    #print word
+    
+#function for scramble sentence
+scramble_word(word)
