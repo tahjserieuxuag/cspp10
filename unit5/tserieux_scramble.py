aa3e4c5987b56aaef12bed68328480b2e5c79100
TahjSerieuxMeme <tahjserieux@uagateway.org>
2017-02-03 17:15:07 +0000

    last index'

--- unit5/tserieux_scramble.py
+++ unit5/tserieux_scramble.py
@@ -1,5 +1,5 @@
 import random
-word = "Paris"
+word = "daddyo"
 #a function that scrambles one word
 def scramble_word(word):
 #turn the word into a list
@@ -8,9 +8,17 @@ def scramble_word(word):
         wlist = list(word)  
         
     #scramble everything but the first and last index
+        i1 = word[:1]
+        il = word[-1:]
+        last = wlist[-1]
         wlist = wlist[1:-1]
+    
         random.shuffle(wlist)
-        print(wlist)
+        wlist.insert(0,i1)
+        wlist.insert([-1],il)
+        final =''.join(wlist)
+        print(final)
+        
     
     #put word back together
         
